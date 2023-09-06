c6b3da6a0aed2a498b4c3a5462981135dd784510
sanjaymulchandani <sunnymulchandani07@gmail.com>
2023-09-06 23:40:55 +0530

    Deployment

--- main/urls.py
+++ main/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin-veripas-sanjay/', admin.site.urls),
     path('', include('app.urls')),
 ]
